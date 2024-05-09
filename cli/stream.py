2b905dcd601b7912c10da76562ccce86b82dd983
zihao.xu <zihao.xu@thehemera.com>
2024-05-09 21:33:00 +0800

    add parameter to set database mode

--- cli/stream.py
+++ cli/stream.py
@@ -12,7 +12,6 @@ from exporters.item_exporter import create_item_exporters
 from utils.thread_local_proxy import ThreadLocalProxy
 
 
-
 @click.command(context_settings=dict(help_option_names=['-h', '--help']))
 @click.option('-l', '--last-synced-block-file', default='last_synced_block.txt', show_default=True, type=str, help='')
 @click.option('--lag', default=0, show_default=True, type=int, help='The number of blocks to lag behind the network.')
@@ -29,23 +28,26 @@ from utils.thread_local_proxy import ThreadLocalProxy
                    'or kafka, output name and connection host:port e.g. kafka/127.0.0.1:9092 '
                    'or Kinesis, e.g. kinesis://your-data-stream-name'
                    'If not specified will print to console')
+@click.option('-m', '--init-mode', default=False, show_default=True, type=bool,
+              help='The database mode to initialize the database.')
 @click.option('-s', '--start-block', default=None, show_default=True, type=int, help='Start block')
 @click.option('-e', '--entity-types', default=','.join(EntityType.ALL_FOR_STREAMING), show_default=True, type=str,
               help='The list of entity types to export.')
-@click.option('--period-seconds', default=10, show_default=True, type=int, help='How many seconds to sleep between syncs')
-@click.option('-b', '--batch-size', default=10, show_default=True, type=int, help='How many blocks to batch in single request')
-@click.option('-B', '--block-batch-size', default=1, show_default=True, type=int, help='How many blocks to batch in single sync round')
+@click.option('--period-seconds', default=10, show_default=True, type=int,
+              help='How many seconds to sleep between syncs')
+@click.option('-b', '--batch-size', default=10, show_default=True, type=int,
+              help='How many blocks to batch in single request')
+@click.option('-B', '--block-batch-size', default=1, show_default=True, type=int,
+              help='How many blocks to batch in single sync round')
 @click.option('-w', '--max-workers', default=5, show_default=True, type=int, help='The number of workers')
 @click.option('--log-file', default=None, show_default=True, type=str, help='Log file')
 @click.option('--pid-file', default=None, show_default=True, type=str, help='pid file')
-def stream(last_synced_block_file, lag, provider_uri, debug_provider_uri, output, start_block, entity_types,
+def stream(last_synced_block_file, lag, provider_uri, debug_provider_uri, output, init_mode, start_block, entity_types,
            period_seconds=10, batch_size=2, block_batch_size=10, max_workers=5, log_file=None, pid_file=None):
     """Streams all data types to console or Google Pub/Sub."""
     configure_logging(log_file)
     configure_signals()
     entity_types = parse_entity_types(entity_types)
-
-
 
     # TODO: Implement fallback mechanism for provider uris instead of picking randomly
     provider_uri = pick_random_provider_uri(provider_uri)
@@ -56,7 +58,7 @@ def stream(last_synced_block_file, lag, provider_uri, debug_provider_uri, output
     streamer_adapter = EthStreamerAdapter(
         batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri(provider_uri, batch=True)),
         batch_web3_debug_provider=ThreadLocalProxy(lambda: get_provider_from_uri(debug_provider_uri, batch=True)),
-        item_exporter=create_item_exporters(output),
+        item_exporter=create_item_exporters(output, init_mode),
         batch_size=batch_size,
         max_workers=max_workers,
         entity_types=entity_types
@@ -82,7 +84,7 @@ def parse_entity_types(entity_types):
         if entity_type not in EntityType.ALL_FOR_STREAMING:
             raise click.BadOptionUsage(
                 '--entity-type', '{} is not an available entity type. Supply a comma separated list of types from {}'
-                    .format(entity_type, ','.join(EntityType.ALL_FOR_STREAMING)))
+                .format(entity_type, ','.join(EntityType.ALL_FOR_STREAMING)))
 
     return entity_types
 
