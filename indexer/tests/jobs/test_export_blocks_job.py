cfab8f1bedeb432afc8dbd7c023c844bee842361
ideal93 <leeideal93@gmail.com>
2024-07-19 20:47:46 +0800

    test

---
+++ indexer/tests/jobs/test_export_blocks_job.py
@@ -0,0 +1,28 @@
+import pytest
+
+from enumeration.entity_type import EntityType
+from indexer.domain.block import Block
+from indexer.exporters.console_item_exporter import ConsoleItemExporter
+from indexer.jobs.job_scheduler import JobScheduler
+from indexer.utils.provider import get_provider_from_uri
+from indexer.utils.thread_local_proxy import ThreadLocalProxy
+
+
+@pytest.mark.export_job
+def test_export_job():
+    job_scheduler = JobScheduler(
+        entity_types=EntityType.BLOCK,
+        batch_web3_provider=ThreadLocalProxy(lambda: get_provider_from_uri("https://ethereum-rpc.publicnode.com", batch=True)),
+        batch_web3_debug_provider=ThreadLocalProxy(lambda: get_provider_from_uri("https://ethereum-rpc.publicnode.com", batch=True)),
+        item_exporters=[ConsoleItemExporter()],
+        batch_size=100,
+        debug_batch_size=1,
+        max_workers=5,
+        config=None
+    )
+
+    job_scheduler.run_jobs(
+        start_block=20273057,
+        end_block=20273058,
+        required_output_types=[Block]
+    )
