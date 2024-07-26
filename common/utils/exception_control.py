efba80d71f64a004b0c05bd2d70024a53332f531
ideal93 <leeideal93@gmail.com>
2024-07-26 18:59:25 +0800

    fix

--- common/utils/exception_control.py
+++ common/utils/exception_control.py
@@ -84,7 +84,10 @@ def decode_response_error(error):
     if code == -32000:
         if message == 'execution reverted' or message == 'out of gas':
             return None
-        raise RPCNotReachable(message)
+        else:
+            # for test
+            # raise RPCNotReachable(message)
+            return None
     elif code == -32700 or code == -32600 or code == -32602:
         raise FastShutdownError(message)
     elif (-32000 > code >= -32099) or code == -32603:
