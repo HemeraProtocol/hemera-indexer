6b6a1fddb28ab7ea3caeb33b8988b7e11aad7614
river <river.zeng@thehemera.com>
2024-08-08 19:18:03 +0800

    add daily address scheduled

--- indexer/daily_aggr_jobs/daily_scheduled_task.py
+++ indexer/daily_aggr_jobs/daily_scheduled_task.py
@@ -1,6 +1,7 @@
 """
 This is a sample timer scheduler.
 """
+
 from datetime import datetime, timedelta
 
 from sqlalchemy import create_engine, text
@@ -18,8 +19,8 @@ def get_yesterday_date():
 
     yesterday_datetime = now - timedelta(days=1)
 
-    today_str = now.strftime('%Y-%m-%d')
-    yesterday_str = yesterday_datetime.strftime('%Y-%m-%d')
+    today_str = now.strftime("%Y-%m-%d")
+    yesterday_str = yesterday_datetime.strftime("%Y-%m-%d")
 
     return today_str, yesterday_str
 
@@ -27,7 +28,7 @@ def get_yesterday_date():
 def get_sql_content(path):
     end_date_str, start_date_str = get_yesterday_date()
 
-    f = open(path, 'r')
+    f = open(path, "r")
     sql_template = f.read()
     sql = sql_template.format(yesterday=start_date_str, today=end_date_str)
     return sql
@@ -36,10 +37,10 @@ def get_sql_content(path):
 pg_url = "your pg url"
 session = create_session(pg_url)
 
-daily_wallet_addresses_aggregates_sql = get_sql_content('./daily_wallet_addresses_aggregates.sql')
+daily_wallet_addresses_aggregates_sql = get_sql_content("./daily_wallet_addresses_aggregates.sql")
 
 session.execute(text(daily_wallet_addresses_aggregates_sql))
 
-period_wallet_addresses_aggregates_sql = get_sql_content('./period_wallet_addresses_aggregates.sql')
+period_wallet_addresses_aggregates_sql = get_sql_content("./period_wallet_addresses_aggregates.sql")
 
 session.execute(text(period_wallet_addresses_aggregates_sql))
