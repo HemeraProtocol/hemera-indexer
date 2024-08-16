5adbef080ca3472c53902ba628e60d2d95fa8595
ideal93 <leeideal93@gmail.com>
2024-08-16 10:38:12 +0800

    Remove dodo test

--- indexer/tests/address_index/test_address_index_job.py
+++ indexer/tests/address_index/test_address_index_job.py
@@ -21,7 +21,7 @@ def test_export_address_index_job():
         batch_size=100,
         debug_batch_size=1,
         max_workers=5,
-        config=None,
+        config={},
         required_output_types=[
             TokenAddressNftInventory,
             AddressTransaction,
