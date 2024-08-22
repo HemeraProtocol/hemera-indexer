5c81660e4c7f9ff17320abb9eaf703d321909ac5
river <river.zeng@thehemera.com>
2024-08-22 13:43:45 +0800

    init

--- common/models/period_feature_uniswap_v3_wallet_address_amount.py
+++ common/models/period_feature_uniswap_v3_wallet_address_amount.py
@@ -7,14 +7,21 @@ from common.models import HemeraModel
 class PeriodFeatureUniswapV3WalletAddressAmount(HemeraModel):
     __tablename__ = "period_feature_uniswap_v3_wallet_address_amount"
 
-    nft_address = Column(BYTEA, primary_key=True, nullable=False)
+    contract_address = Column(BYTEA, primary_key=True, nullable=False)
     period_date = Column(DATE, primary_key=True, nullable=False)
     token_id = Column(INTEGER, primary_key=True, nullable=False)
     wallet_address = Column(BYTEA, nullable=False)
+    token0_address = Column(String, nullable=False)
     token0_symbol = Column(String, nullable=False)
-    amount0 = Column(NUMERIC(78))
+    token0_balance = Column(NUMERIC(78))
+    token0_price = Column(NUMERIC(78))
+    token0_price_value = Column(NUMERIC(78))
+
+    token1_address = Column(String, nullable=False)
     token1_symbol = Column(String, nullable=False)
-    amount1 = Column(NUMERIC(78))
+    token1_balance = Column(NUMERIC(78))
+    token1_price = Column(NUMERIC(78))
+    token1_price_value = Column(NUMERIC(78))
 
 
 # could be replaced by partition in case of huge amount data
