d21252ff0397b8ed120573afad15dd689091086f
river <river.zeng@thehemera.com>
2024-08-23 17:36:58 +0800

    init

---
+++ common/models/daily_feature_staked_fbtc_detail_records.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Index, DATE
+from sqlalchemy.dialects.postgresql import BYTEA, NUMERIC, VARCHAR
+
+from common.models import HemeraModel
+
+
+class DailyFeatureStakedFBTCDetailRecords(HemeraModel):
+    __tablename__ = "daily_feature_staked_fbtc_detail_records"
+    contract_address = Column(BYTEA, primary_key=True)
+    wallet_address = Column(BYTEA, primary_key=True)
+    block_date = Column(DATE, primary_key=True, nullable=False)
+    amount = Column(NUMERIC(100))
+    protocol_id = Column(VARCHAR)
+
+
+Index(
+    "daily_feature_staked_fbtc_detail_records_wallet_block_date",
+    DailyFeatureStakedFBTCDetailRecords.block_date
+)
---
+++ common/models/period_feature_staked_fbtc_detail_records.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Index, DATE
+from sqlalchemy.dialects.postgresql import BYTEA, NUMERIC, VARCHAR
+
+from common.models import HemeraModel
+
+
+class PeriodFeatureStakedFBTCDetailRecords(HemeraModel):
+    __tablename__ = "period_feature_staked_fbtc_detail_records"
+    contract_address = Column(BYTEA, primary_key=True)
+    wallet_address = Column(BYTEA, primary_key=True)
+    period_date = Column(DATE, primary_key=True, nullable=False)
+    amount = Column(NUMERIC(100))
+    protocol_id = Column(VARCHAR)
+
+
+Index(
+    "period_feature_staked_fbtc_detail_records_wallet_period_date",
+    PeriodFeatureStakedFBTCDetailRecords.period_date
+)
