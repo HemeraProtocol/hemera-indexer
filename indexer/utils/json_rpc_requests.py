10cf4fb1cd13be36cc51b960ffc5a0c4b125572a
li xiang <leeideal93@gmail.com>
2024-08-28 19:12:27 +0800

    Fix set trace rpc request id (#81)
    * Fix trace rpc request id

--- indexer/utils/json_rpc_requests.py
+++ indexer/utils/json_rpc_requests.py
@@ -13,7 +13,7 @@ def generate_trace_block_by_number_json_rpc(block_numbers):
             method="debug_traceBlockByNumber",
             params=[block_number, {"tracer": "callTracer"}],
             # save block_number in request ID, so later we can identify block number in response
-            request_id=block_number,
+            request_id=int(block_number, 16),
         )
 
 
