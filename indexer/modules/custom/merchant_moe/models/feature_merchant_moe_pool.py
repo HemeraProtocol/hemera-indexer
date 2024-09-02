1abba7bb1c86d128f0571f4126f7e56d28519a5a
knight <yangyang.wang@lifo.ai>
2024-09-02 13:28:09 +0800

    update merchant moe

--- indexer/modules/custom/merchant_moe/models/feature_merchant_moe_pool.py
+++ indexer/modules/custom/merchant_moe/models/feature_merchant_moe_pool.py
@@ -9,8 +9,8 @@ from common.models import HemeraModel, general_converter
 class FeatureMerChantMoePools(HemeraModel):
     __tablename__ = "feature_merchant_moe_pools"
     token_address = Column(BYTEA, primary_key=True)
-    block_timestamp = Column(BIGINT, primary_key=True)
-    block_number = Column(BIGINT, primary_key=True)
+    block_timestamp = Column(BIGINT)
+    block_number = Column(BIGINT)
     token0_address = Column(BYTEA)
     token1_address = Column(BYTEA)
 
