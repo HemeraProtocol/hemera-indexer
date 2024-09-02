b51eb6cf94e58cf96e17e40d56887dd0c3f5e7ce
river <river.zeng@thehemera.com>
2024-09-02 20:58:49 +0800

    improve dodo logic

--- indexer/aggr_jobs/order_jobs/period_feature_holding_balance_dodo.sql
+++ indexer/aggr_jobs/order_jobs/period_feature_holding_balance_dodo.sql
@@ -3,6 +3,8 @@ from period_feature_holding_balance_dodo
 where period_date >= '{start_date}'
   and period_date < '{end_date}';
 
+
+
 WITH supply_cte AS (SELECT total_supply, 30373474 as base_fee, 14587260 as quote_fee
                     FROM period_feature_erc20_token_supply_records
                     WHERE period_date = '{start_date}'
@@ -20,28 +22,40 @@ WITH supply_cte AS (SELECT total_supply, 30373474 as base_fee, 14587260 as quote
                      where period_date = '{start_date}'
                        and token_address = '\xD39DFbfBA9E7eccd813918FfbDa10B783EA3b3C6'),
 
-     fbtc_and_wbtc_balance_table as (select d1.address,
-                                            d2.address            as base_token_address,
-                                            d2.symbol             as base_token_symbol,
-                                            d2.decimals           as base_token_decimals,
-                                            d3.address            as quote_token_address,
-                                            d3.symbol             as quote_token_symbol,
-                                            d3.decimals           as quote_token_decimals,
-                                            d1.balance,
-                                            d1.balance - 30373474 as base_balacne,
-                                            d1.balance - 14587260 as quote_balance,
-                                            d1.token_address,
-                                            case
-                                                when token_address = '\xc96de26018a54d51c097160568752c4e3bd6c364' then 1
-                                                else 0 end        as token_flag
+     address_balance as (select d1.address,
+                                d1.token_address as token0_address,
+                                d1.balance       as token0_balance,
+                                d2.token_address as token1_address,
+                                d2.balance       as token1_balance
+                         from (select *
+                               from period_address_token_balances
+                               where period_date = '{start_date}'
+                                 and address = '\xD39DFbfBA9E7eccd813918FfbDa10B783EA3b3C6'
+                                 and token_address = '\xc96de26018a54d51c097160568752c4e3bd6c364') d1
+                                  inner join
+                              (select *
+                               from period_address_token_balances
+                               where period_date = '{start_date}'
+                                 and address = '\xD39DFbfBA9E7eccd813918FfbDa10B783EA3b3C6'
+                                 and token_address = '\xCAbAE6f6Ea1ecaB08Ad02fE02ce9A44F09aebfA2') d2
+                              on d1.address = d2.address),
 
-                                     from period_address_token_balances d1
-                                              inner join tokens d2 on d2.address = '\xc96de26018a54d51c097160568752c4e3bd6c364'
-                                              inner join tokens d3 on d3.address = '\xCAbAE6f6Ea1ecaB08Ad02fE02ce9A44F09aebfA2'
-                                     where period_date = '{start_date}'
-                                       and d1.address = '\xD39DFbfBA9E7eccd813918FfbDa10B783EA3b3C6'
-                                       and token_address in ('\xc96de26018a54d51c097160568752c4e3bd6c364',
-                                                             '\xCAbAE6f6Ea1ecaB08Ad02fE02ce9A44F09aebfA2'))
+
+     fbtc_and_wbtc_balance_table as (select d1.address,
+                                            d2.address                   as base_token_address,
+                                            d2.symbol                    as base_token_symbol,
+                                            d2.decimals                  as base_token_decimals,
+                                            d3.address                   as quote_token_address,
+                                            d3.symbol                    as quote_token_symbol,
+                                            d3.decimals                  as quote_token_decimals,
+
+                                            d1.token0_balance - 30373474 as base_balance,
+                                            d1.token0_balance - 14587260 as quote_balance
+
+                                     from address_balance d1
+                                              inner join tokens d2 on d1.token0_address = d2.address
+                                              inner join tokens d3 on d1.token0_address = d3.address)
+
 insert
 into period_feature_holding_balance_dodo (period_date, protocol_id, contract_address, wallet_address, balance_of,
                                           total_supply, token0_address, token0_symbol, token0_balance,
@@ -54,17 +68,17 @@ select d1.period_date,
        d1.total_supply,
        d2.base_token_address,
        d2.base_token_symbol,
-       case
-           when token_flag = 0 then rate * base_balacne / pow(10, base_token_decimals) end as base_token_balance,
+       rate * base_balance / pow(10, base_token_decimals)      base_token_balance,
        d2.quote_token_address,
        d2.base_token_symbol,
-       case
-           when token_flag = 1
-               then rate * quote_balance / pow(10, quote_token_decimals) end               as quote_token_balance
+       rate * quote_balance / pow(10, quote_token_decimals) as quote_token_balance
 
 from gcb_balance d1
          inner join
-     fbtc_and_wbtc_balance_table d2 on d1.address = d2.address
+     fbtc_and_wbtc_balance_table d2 on d1.token_address = d2.address
 ;
+
+
+
 
 
