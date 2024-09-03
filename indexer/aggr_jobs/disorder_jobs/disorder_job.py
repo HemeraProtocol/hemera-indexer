5f930cf637942a4a896bc00f84e5a93bcef3b7d3
river <river.zeng@thehemera.com>
2024-09-03 14:36:16 +0800

    fix order bug

--- indexer/aggr_jobs/disorder_jobs/disorder_job.py
+++ indexer/aggr_jobs/disorder_jobs/disorder_job.py
@@ -5,7 +5,7 @@ from indexer.executors.batch_work_executor import BatchWorkExecutor
 
 job_list = ['daily_address_token_balances', 'daily_feature_holding_balance_staked_fbtc_detail.sql',
             'daily_feature_holding_balance_uniswap_v3.sql', 'daily_feature_erc1155_token_holdings.sql',
-            'daily_feature_erc20_token_supply_records.sql'
+            'daily_feature_erc20_token_supply_records.sql',
             'daily_feature_erc1155_token_supply_records.sql']
 
 
