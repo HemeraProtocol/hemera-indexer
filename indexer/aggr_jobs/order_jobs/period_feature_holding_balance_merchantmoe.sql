fafc5fab2312f6854bdd2dfe095e5e410dc9704a
river <river.zeng@thehemera.com>
2024-09-03 16:11:46 +0800

    improve mechab moe sql

--- indexer/aggr_jobs/order_jobs/period_feature_holding_balance_merchantmoe.sql
+++ indexer/aggr_jobs/order_jobs/period_feature_holding_balance_merchantmoe.sql
@@ -16,25 +16,38 @@ delete
 from period_feature_holding_balance_merchantmoe
 where period_date >= '{start_date}'
   and period_date < '{end_date}';
-with detail_table as (select d1.wallet_address,
-                             d1.token_address,
-                             d1.token_id,
-                             d1.balance,
-                             d2.total_supply,
-                             d3.reserve0_bin,
-                             d3.reserve1_bin,
---                              concat('0x', encode(d4.address, 'hex')) as token0_address,
---                              concat('0x', encode(d5.address, 'hex')) as token1_address,
-                             d4.address  as token0_address,
-                             d5.address  as token1_address,
+insert
+into period_feature_holding_balance_merchantmoe(period_date, protocol_id, contract_address, token_id,
+                                                wallet_address, token0_address, token0_symbol, token0_balance,
+                                                token1_address, token1_symbol, token1_balance)
+with moe_pools_table as (select d0.*,
 
-                             d4.symbol   as token0_symbol,
-                             d4.decimals as token0_decimals,
-                             d5.symbol   as token1_symbol,
-                             d5.decimals as token1_decimals
-                      from feature_merchant_moe_pools d0
+                                d4.symbol   as token0_symbol,
+                                d4.decimals as token0_decimals,
+                                d5.symbol   as token1_symbol,
+                                d5.decimals as token1_decimals
+                         from feature_merchant_moe_pools d0
+                                  inner join tokens d4 on d0.token0_address = d4.address
+                                  inner join tokens d5 on d0.token1_address = d5.address
+                         where d4.symbol = 'FBTC'
+                            or d5.symbol = 'FBTC'),
+
+     detail_table as (select d1.wallet_address
+                           , d1.token_address
+                           , d1.token_id
+                           , d1.balance
+                           , d2.total_supply
+                           , d3.reserve0_bin
+                           , d3.reserve1_bin
+                           , token0_address
+                           , token0_symbol
+                           , token0_decimals
+                           , token1_address
+                           , token1_symbol
+                           , token1_decimals
+
+                      from moe_pools_table d0
                                inner join
-
                            (select * from period_feature_erc1155_token_holdings where period_date = '{start_date}') d1
                            on d0.token_address = d1.token_address
                                inner join
@@ -45,15 +58,7 @@ with detail_table as (select d1.wallet_address,
                                inner join (select *
                                            from period_feature_merchant_moe_token_bin_records
                                            where period_date = '{start_date}') d3
-                                          on d1.token_address = d3.token_address and d1.token_id = d3.token_id
-                               inner join tokens d4 on d0.token0_address = d4.address
-                               inner join tokens d5 on d0.token1_address = d5.address)
-
-
-insert
-into period_feature_holding_balance_merchantmoe(period_date, protocol_id, contract_address, token_id,
-                                                wallet_address, token0_address, token0_symbol, token0_balance,
-                                                token1_address, token1_symbol, token1_balance)
+                                          on d1.token_address = d3.token_address and d1.token_id = d3.token_id)
 
 select date('{start_date}'),
        'merchantmoe'  as protocol_id,
@@ -71,8 +76,6 @@ select date('{start_date}'),
            when total_supply > 0 then (balance / total_supply) * reserve1_bin / pow(10, token1_decimals)
            else 0 end as token1_balance
 from detail_table
-where token0_symbol = 'FBTC'
-   or token1_symbol = 'FBTC'
 ;
 
 
