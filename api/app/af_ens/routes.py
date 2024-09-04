bc5f6130c29693a3adbcd576b1a9ee555f913548
will0x0909 <zhiyuan@thehemera.com>
2024-09-04 14:32:56 +0800

    ens api

--- api/app/af_ens/routes.py
+++ api/app/af_ens/routes.py
@@ -8,9 +8,11 @@ from common.models import db
 from common.models.af_ens_address_current import ENSAddress
 from common.models.af_ens_event import ENSMiddle
 from common.models.af_ens_node_current import ENSRecord
+from common.models.erc721_token_id_changes import ERC721TokenIdChanges
 from common.utils.config import get_config
 
 app_config = get_config()
+from sqlalchemy import or_, and_, desc, func
 
 
 af_ens_namespace = Namespace("User Operation Namespace", path="/", description="ENS feature")
@@ -31,22 +33,26 @@ class ExplorerUserOperationDetails(Resource):
         else:
             return res
 
-        all_records_rows = db.session.query(ENSRecord).filter(ENSRecord.address == address).all()
+        # current_address holds 721 & 1155 tokens
+        all_721_owns = db.session.query(ERC721TokenIdChanges).filter(ERC721TokenIdChanges.token_owner == address).all()
+        all_721_ids = [r.token_id for r in all_721_owns]
+        all_owned = db.session.query(ENSRecord).filter(ENSRecord.token_id.in_(all_721_ids)).all()
+        res['ens_holdings'] = [r.name for r in all_owned]
+        # TODO support 1155
+        # all_1155_owns = db.session.query(ENSRecord).filter(ENSRecord.address == address).all()
 
         primary_address_row = db.session.query(ENSAddress).filter(ENSAddress.address == address).first()
-
         if primary_address_row:
             primary_record = db.session.query(ENSRecord).filter(ENSRecord.name == primary_address_row.name).first()
             if primary_record:
                 res["primary_name"] = primary_record.name
-                res["first_set_primary_name"] = datetime_to_string(primary_record.registration)
 
-        if all_records_rows:
-            res["ens_owned"] = [record.name for record in all_records_rows]
-            res["first_own_ens"] = datetime_to_string(min(record.registration for record in all_records_rows))
+        be_resolved_ens = db.session.query(ENSAddress).filter(ENSAddress.address == address).all()
+        res['be_resolved_by_ens'] = [r.name for r in be_resolved_ens]
 
-        resolved_records = db.session.query(ENSRecord).filter(ENSRecord.address == address).all()
-        res["resolved_to_ens"] = [record.name for record in resolved_records]
+        first_register = db.session.query(ENSMiddle).filter(and_(ENSMiddle.from_address == address, ENSMiddle.event_name == 'NameRegistered')).first()
+        if first_register:
+            res['first_register_time'] = datetime_to_string(first_register.block_timestamp)
 
         return res
 
@@ -54,18 +60,28 @@ class ExplorerUserOperationDetails(Resource):
 @af_ens_namespace.route("/v1/aci/<address>/ens/detail")
 class ExplorerUserOperationDetails(Resource):
     def get(self, address):
-        res = {
-
-        }
+        lis = []
         if address:
             address = bytes.fromhex(address[2:])
         else:
-            return res
+            return lis
 
+        all_records_rows = db.session.query(ENSMiddle).filter(ENSMiddle.from_address == address).order_by(ENSMiddle.block_number, ENSMiddle.transaction_index, ENSMiddle.log_index.desc()).all()
+        all_ens_records = db.session.query(ENSRecord).filter(ENSRecord.node.in_([r.node for r in all_records_rows])).all()
+        node_name_map = {r.node: r.name for r in all_ens_records if r.name}
+        for r in all_records_rows:
+            lis.append({
+                'method': r.method,
+                'event': r.event_name,
+                'block_number': r.block_number,
+                'transaction_index': r.transaction_index,
+                'log_index': r.log_index,
+                'transaction_hash': '0x' + r.transaction_hash.hex(),
+                'node': '0x' + r.node.hex(),
+                'name': node_name_map.get(r.node)
+            })
 
-        all_records_rows = db.session.query(ENSMiddle).filter(ENSMiddle.from_address == address).all()
-
-        return [model_to_dict(a) for a in all_records_rows]
+        return lis
 
 
 def datetime_to_string(dt, format="%Y-%m-%d %H:%M:%S"):
