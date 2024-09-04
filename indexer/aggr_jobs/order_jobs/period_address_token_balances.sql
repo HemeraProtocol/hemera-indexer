43c64ec983ff0c2e60f0786d78a0b93242d34045
river <river.zeng@thehemera.com>
2024-09-04 20:39:35 +0800

    fix bug of period_address_token_balances.sql

--- indexer/aggr_jobs/order_jobs/period_address_token_balances.sql
+++ indexer/aggr_jobs/order_jobs/period_address_token_balances.sql
@@ -2,6 +2,9 @@ INSERT INTO period_address_token_balances (address, period_date, token_address, 
 SELECT d1.address, d1.block_date, d1.token_address, d1.token_id, d1.token_type, d1.balance
 FROM daily_address_token_balances d1
 where block_date = '{start_date}'
-ON CONFLICT (address, period_date, token_address, token_id)
+ON CONFLICT (address, token_address, token_id)
     DO UPDATE
-    SET balance = EXCLUDED.balance;
+    SET balance     = EXCLUDED.balance,
+        token_type  = EXCLUDED.token_type,
+        period_date = EXCLUDED.period_date
+;
