dbc03e2e7804d6a0b3c3463fd1a20f6b977d9711
river <river.zeng@thehemera.com>
2024-09-04 19:38:22 +0800

    fix start_date logic

--- indexer/aggr_jobs/order_jobs/period_address_token_balances.sql
+++ indexer/aggr_jobs/order_jobs/period_address_token_balances.sql
@@ -1,7 +1,7 @@
 INSERT INTO period_address_token_balances (address, period_date, token_address, token_id, token_type, balance)
 SELECT d1.address, d1.block_date, d1.token_address, d1.token_id, d1.token_type, d1.balance
 FROM daily_address_token_balances d1
-where block_date = '{block_date}'
+where block_date = '{start_date}'
 ON CONFLICT (address, period_date, token_address, token_id)
     DO UPDATE
     SET balance = EXCLUDED.balance;
