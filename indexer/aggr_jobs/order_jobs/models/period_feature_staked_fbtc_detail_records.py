b3439dcbe497de95e2618770a683cf9dcf54f6cc
river <river.zeng@thehemera.com>
2024-09-09 16:25:07 +0800

    add staked daily jobs

---
+++ indexer/aggr_jobs/order_jobs/models/period_feature_staked_fbtc_detail_records.py
@@ -0,0 +1,20 @@
+from sqlalchemy import DATE, TIMESTAMP, Column, Index, func
+from sqlalchemy.dialects.postgresql import BYTEA, NUMERIC, VARCHAR
+
+from common.models import HemeraModel
+
+
+class PeriodFeatureStakedFBTCDetailRecords(HemeraModel):
+    __tablename__ = "period_feature_staked_fbtc_detail_records"
+
+    period_date = Column(DATE, primary_key=True, nullable=False)
+    contract_address = Column(BYTEA, primary_key=True)
+    wallet_address = Column(BYTEA, primary_key=True)
+
+    amount = Column(NUMERIC(100))
+    protocol_id = Column(VARCHAR)
+
+    create_time = Column(TIMESTAMP, server_default=func.now())
+
+
+Index("period_feature_staked_fbtc_detail_records_wallet_period_date", PeriodFeatureStakedFBTCDetailRecords.period_date)
