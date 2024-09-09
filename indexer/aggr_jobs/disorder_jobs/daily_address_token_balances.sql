b35ffa3d100d7bbcd88f863d4da7532548c4e66c
river <river.zeng@thehemera.com>
2024-09-09 16:29:36 +0800

    add dodo daily jobs

---
+++ indexer/aggr_jobs/disorder_jobs/daily_address_token_balances.sql
@@ -0,0 +1,21 @@
+delete
+from daily_address_token_balances
+WHERE block_date >= '{start_date}'
+  and block_date < '{end_date}';
+
+insert into public.daily_address_token_balances (address, block_date, token_address, token_id, token_type, balance
+                                                 )
+
+select address,
+       date(block_timestamp) as block_date,
+       token_address,
+       token_id,
+       token_type,
+       balance
+from (select *,
+             row_number() over (partition by address,token_address,token_id order by block_timestamp desc) as rn
+      from address_token_balances
+      WHERE block_timestamp >= '{start_date}'
+        and block_timestamp < '{end_date}') t
+where rn = 1;
+
