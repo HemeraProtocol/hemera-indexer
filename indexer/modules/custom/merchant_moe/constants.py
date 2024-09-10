f4d2fa27a70f7b22894ebdfdd20a5da61f2b7a0a
knight <yangyang.wang@lifo.ai>
2024-09-10 15:50:26 +0800

    add

--- indexer/modules/custom/merchant_moe/constants.py
+++ indexer/modules/custom/merchant_moe/constants.py
@@ -33,7 +33,10 @@ ABI_LIST = [
 ]
 
 LIQUIDITY_LIST = [
+    # DepositedToBins
     "0x87f1f9dcf5e8089a3e00811b6a008d8f30293a3da878cb1fe8c90ca376402f8a",
+    # WithdrawnFromBins
     "0xa32e146844d6144a22e94c586715a1317d58a8aa3581ec33d040113ddcb24350",
+    # TransferBatch
     "0x4a39dc06d4c0dbc64b70af90fd698a233a518aa5d07e595d983b8c0526c8f7fb",
 ]
