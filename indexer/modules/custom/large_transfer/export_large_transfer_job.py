d6cca0ea869686261bfc5df94c2e153fda8ea5f7
will0x0909 <zhiyuan@thehemera.com>
2024-09-11 15:03:33 +0800

    format code

--- indexer/modules/custom/large_transfer/export_large_transfer_job.py
+++ indexer/modules/custom/large_transfer/export_large_transfer_job.py
@@ -40,184 +40,123 @@ class LargeTransferJob(ExtensionJob):
             kwargs["max_workers"],
             job_name=self.__class__.__name__,
         )
-        self.limit_eth = self.user_defined_config.get("eth") * (10**18)
-
+        self.limit_eth = self.get_eth_limit(self.user_defined_config)
         self.rules = self.user_defined_config.get("rules")
-
-        if self.limit_eth is None or len(self.rules) == 0:
-            raise FastShutdownError("LargeTransferJob limit config is empty")
+        self.validate_config()
 
         self._is_batch = kwargs["batch_size"] > 1
         self._filters = kwargs.get("filters", [])
-
         self.db_service = kwargs["config"].get("db_service")
+
+    @staticmethod
+    def get_eth_limit(config):
+        return config.get("eth", 0) * (10**18)
+
+    def validate_config(self):
+        if self.limit_eth is None or not self.rules:
+            raise FastShutdownError("LargeTransferJob limit config is empty")
 
     def _collect(self, **kwargs):
         transactions: List[Transaction] = self._data_buff.get(Transaction.type(), [])
         token_transfers = self._data_buff.get(ERC20TokenTransfer.type(), [])
-        transactions_map = {}
-        group_data = defaultdict(list)
-        for ta in transactions:
-            transactions_map[ta.hash] = ta
-            group_data[ta.hash] = []
-        for tf in token_transfers:
-            group_data[tf.transaction_hash].append(tf)
-        block_number = max(tra.block_number for tra in transactions)
-        res = []
-        address_token_in = defaultdict(lambda: defaultdict(int))
-        address_token_out = defaultdict(lambda: defaultdict(int))
-        for tnx, tfs in group_data.items():
-            tra = transactions_map.get(tnx)
-            if tra.value > self.limit_eth:
-                res.append(
-                    LargeTransferTransactionD(
-                        transaction_hash=tra.hash,
-                        transaction_index=tra.transaction_index,
-                        from_address=tra.from_address,
-                        to_address=tra.to_address,
-                        value=tra.value,
-                        transaction_type=tra.transaction_type,
-                        input=tra.input,
-                        nonce=tra.nonce,
-                        block_hash=tra.block_hash,
-                        block_number=tra.block_number,
-                        block_timestamp=tra.block_timestamp,
-                    )
-                )
 
-                address_token_in[tra.to_address][ETH] += tra.value
-                address_token_in[tra.to_address][TC] += 1
-                address_token_out[tra.from_address][ETH] += tra.value
-                address_token_out[tra.from_address][TC] += 1
-
-            else:
-                large_flag = False
-                for tf in tfs:
-                    for rule in self.rules:
-                        if tf.token_address == rule["token_address"] and tf.value > (rule["limit"] * 10**6):
-                            large_flag = True
-                            address_token_in[tf.to_address][tf.token_address] += tf.value
-                            address_token_in[tf.to_address][TC] += 1
-                            address_token_out[tf.from_address][tf.token_address] += tf.value
-                            address_token_out[tf.from_address][TC] += 1
-                            break
-
-                if large_flag:
-                    res.append(
-                        LargeTransferTransactionD(
-                            transaction_hash=tra.hash,
-                            transaction_index=tra.transaction_index,
-                            from_address=tra.from_address,
-                            to_address=tra.to_address,
-                            value=tra.value,
-                            transaction_type=tra.transaction_type,
-                            input=tra.input,
-                            nonce=tra.nonce,
-                            block_hash=tra.block_hash,
-                            block_number=tra.block_number,
-                            block_timestamp=tra.block_timestamp,
-                        )
-                    )
-        # query exists
-        address_lis = list(set(address_token_in.keys()) | set(address_token_out.keys()))
-        token_lis = list(set(get_second_level_keys(address_token_in)) | set(get_second_level_keys(address_token_out)))
-        exists_dic = get_exist_large_transfers(self.db_service, address_lis, token_lis)
-        # update value
-        lafs = []
-        for ad, token_balance_dic in address_token_in.items():
-            for tk, balance in token_balance_dic.items():
-                k = build_k(ad, tk)
-                if tk == TC:
-                    if k in exists_dic:
-                        exists_dic[k].transaction_count += balance
-                    # else:
-                    #     exists_dic[k] = LargeTransferAddressD(
-                    #         address=ad,
-                    #         token_address=tk,
-                    #         transaction_count=balance,
-                    #         amount_in=0,
-                    #         amount_out=0,
-                    #         block_number=block_number
-                    #     )
-                else:
-                    if k in exists_dic:
-                        exists_dic[k].amount_in += balance
-                    else:
-                        exists_dic[k] = LargeTransferAddressD(
-                            address=ad,
-                            token_address=tk,
-                            transaction_count=1,
-                            amount_in=balance,
-                            amount_out=0,
-                            block_number=block_number,
-                        )
-
-        for ad, token_balance_dic in address_token_out.items():
-            for tk, balance in token_balance_dic.items():
-                k = build_k(ad, tk)
-                if tk == TC:
-                    if k in exists_dic:
-                        exists_dic[k].transaction_count += balance
-                    # else:
-                    #     exists_dic[k] = LargeTransferAddressD(
-                    #         address=ad,
-                    #         token_address=tk,
-                    #         transaction_count=balance,
-                    #         amount_in=0,
-                    #         amount_out=0,
-                    #         block_number=block_number
-                    #     )
-                else:
-                    if k in exists_dic:
-                        exists_dic[k].amount_out += balance
-                    else:
-                        exists_dic[k] = LargeTransferAddressD(
-                            address=ad,
-                            token_address=tk,
-                            transaction_count=1,
-                            amount_in=0,
-                            amount_out=balance,
-                            block_number=block_number,
-                        )
-        # for item in res + list(exists_dic.values()):
-        for item in list(exists_dic.values()):
-
+        results, address_token_in, address_token_out = self.process(transactions, token_transfers)
+        block_number = max(tx.block_number for tx in transactions)
+        updated_transfers = self.update_large_transfers(address_token_in, address_token_out, block_number)
+        for item in results + updated_transfers:
             if item:
                 self._collect_item(item.type(), item)
 
+    def process(self, transactions, token_transfers):
+        transactions_map = {tx.hash: tx for tx in transactions}
+        group_data = self.group_transfers_by_transaction(token_transfers)
 
-def get_second_level_keys(nested_dict):
-    second_level_keys = set()
-    for value in nested_dict.values():
-        if isinstance(value, dict):
-            second_level_keys.update(value.keys())
-    return list(second_level_keys)
+        results = []
+        address_token_in = defaultdict(lambda: defaultdict(int))
+        address_token_out = defaultdict(lambda: defaultdict(int))
 
+        for tx_hash, transfers in group_data.items():
+            tx = transactions_map.get(tx_hash)
+            if not tx:
+                continue
 
-def get_exist_large_transfers(db_service, address_lis, token_address_lis):
-    if not db_service:
-        return {}
+            if self.is_large_eth_transfer(tx):
+                results.append(self.create_large_transfer_transaction(tx))
+                self.update_address_balances(address_token_in, address_token_out, tx, ETH)
+            elif self.is_large_token_transfer(transfers):
+                results.append(self.create_large_transfer_transaction(tx))
+                self.update_token_balances(address_token_in, address_token_out, transfers)
 
-    session = db_service.get_service_session()
-    try:
-        address_lis = [addr.encode("utf-8") if isinstance(addr, str) else addr for addr in address_lis]
-        token_address_lis = [
-            tok_addr.encode("utf-8") if isinstance(tok_addr, str) else tok_addr for tok_addr in token_address_lis
-        ]
+        return results, address_token_in, address_token_out
 
-        result = (
-            session.query(LargeTransferAddress)
-            .filter(
-                and_(
-                    LargeTransferAddress.address.in_(address_lis),
-                    LargeTransferAddress.token_address.in_(token_address_lis),
-                )
-            )
-            .all()
+    @staticmethod
+    def group_transfers_by_transaction(token_transfers):
+        group_data = defaultdict(list)
+        for tf in token_transfers:
+            group_data[tf.transaction_hash].append(tf)
+        return group_data
+
+    def is_large_eth_transfer(self, tx):
+        return tx.value > self.limit_eth
+
+    def is_large_token_transfer(self, transfers):
+        return any(
+            tf.value > (rule["limit"] * 10**6)
+            for tf in transfers
+            for rule in self.rules
+            if tf.token_address == rule["token_address"]
         )
-        res = {}
-        for row in result:
-            res[build_k(row.address, row.token_address)] = LargeTransferAddressD(
+
+    @staticmethod
+    def create_large_transfer_transaction(tx):
+        return LargeTransferTransactionD(
+            transaction_hash=tx.hash,
+            transaction_index=tx.transaction_index,
+            from_address=tx.from_address,
+            to_address=tx.to_address,
+            value=tx.value,
+            transaction_type=tx.transaction_type,
+            input=tx.input,
+            nonce=tx.nonce,
+            block_hash=tx.block_hash,
+            block_number=tx.block_number,
+            block_timestamp=tx.block_timestamp,
+        )
+
+    @staticmethod
+    def update_address_balances(address_token_in, address_token_out, tx, token):
+        address_token_in[tx.to_address][token] += tx.value
+        address_token_in[tx.to_address][TC] += 1
+        address_token_out[tx.from_address][token] += tx.value
+        address_token_out[tx.from_address][TC] += 1
+
+    def update_token_balances(self, address_token_in, address_token_out, transfers):
+        for tf in transfers:
+            for rule in self.rules:
+                if tf.token_address == rule["token_address"] and tf.value > (rule["limit"] * 10**6):
+                    self.update_address_balances(address_token_in, address_token_out, tf, tf.token_address)
+                    break
+
+    def get_existing_large_transfers(self, addresses, tokens):
+        if not self.db_service:
+            return {}
+
+        addresses = [addr.encode("utf-8") if isinstance(addr, str) else addr for addr in addresses]
+        tokens = [tok_addr.encode("utf-8") if isinstance(tok_addr, str) else tok_addr for tok_addr in tokens]
+        with self.db_service.get_service_session() as session:
+            result = (
+                session.query(LargeTransferAddress)
+                .filter(
+                    and_(
+                        LargeTransferAddress.address.in_(addresses),
+                        LargeTransferAddress.token_address.in_(tokens),
+                    )
+                )
+                .all()
+            )
+
+        return {
+            f"{row.address}.{row.token_address}": LargeTransferAddressD(
                 address=row.address,
                 token_address=row.token_address,
                 transaction_count=row.transaction_count,
@@ -225,14 +164,45 @@ def get_exist_large_transfers(db_service, address_lis, token_address_lis):
                 amount_out=row.amount_out,
                 block_number=row.block_number,
             )
+            for row in result
+        }
 
-    except Exception as e:
-        raise e
-    finally:
-        session.close()
+    def update_large_transfers(self, address_token_in, address_token_out, block_number):
+        addresses = set(address_token_in.keys()) | set(address_token_out.keys())
+        tokens = set(self.get_second_level_keys(address_token_in)) | set(self.get_second_level_keys(address_token_out))
 
-    return res
+        existing_transfers = self.get_existing_large_transfers(addresses, tokens)
 
+        for address, token_balance in address_token_in.items():
+            self.update_transfer_record(existing_transfers, address, token_balance, block_number, is_inbound=True)
 
-def build_k(address, token_address):
-    return address + "." + token_address
+        for address, token_balance in address_token_out.items():
+            self.update_transfer_record(existing_transfers, address, token_balance, block_number, is_inbound=False)
+
+        return list(existing_transfers.values())
+
+    @staticmethod
+    def update_transfer_record(existing_transfers, address, token_balance, block_number, is_inbound):
+        for token, balance in token_balance.items():
+            key = f"{address}.{token}"
+            if key in existing_transfers:
+                record = existing_transfers[key]
+                record.transaction_count += balance if token == TC else 1
+                if token != TC:
+                    if is_inbound:
+                        record.amount_in += balance
+                    else:
+                        record.amount_out += balance
+            elif token != TC:
+                existing_transfers[key] = LargeTransferAddressD(
+                    address=address,
+                    token_address=token,
+                    transaction_count=1,
+                    amount_in=balance if is_inbound else 0,
+                    amount_out=balance if not is_inbound else 0,
+                    block_number=block_number,
+                )
+
+    @staticmethod
+    def get_second_level_keys(nested_dict):
+        return {key for subdict in nested_dict.values() for key in subdict}
