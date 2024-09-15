0e8ea34454288b9b23393c1823d0ab8607a3a3c4
ideal93 <leeideal93@gmail.com>
2024-09-15 14:22:23 +0800

    Fix

--- indexer/modules/custom/address_index/models/address_index_daily_stats.py
+++ indexer/modules/custom/address_index/models/address_index_daily_stats.py
@@ -1,9 +1,7 @@
-from datetime import datetime
+from sqlalchemy import DATE, Column
+from sqlalchemy.dialects.postgresql import BIGINT, BYTEA, INTEGER, NUMERIC
 
-from sqlalchemy import DATE, Column, Index, desc, func
-from sqlalchemy.dialects.postgresql import BIGINT, BOOLEAN, BYTEA, INTEGER, NUMERIC, SMALLINT, TEXT, TIMESTAMP
-
-from common.models import HemeraModel, general_converter
+from common.models import HemeraModel
 
 
 class AddressIndexDailyStats(HemeraModel):
@@ -46,23 +44,11 @@ class AddressIndexDailyStats(HemeraModel):
 
     transaction_count = Column(
         INTEGER,
-        generated=True,
-        server_default=func.coalesce(transaction_in_count + transaction_out_count + transaction_self_count, 0),
     )
     internal_transaction_count = Column(
         INTEGER,
-        generated=True,
-        server_default=func.coalesce(
-            internal_transaction_in_count + internal_transaction_out_count + internal_transaction_self_count, 0
-        ),
     )
     erc20_transfer_count = Column(
         INTEGER,
-        generated=True,
-        server_default=func.coalesce(erc20_transfer_in_count + erc20_transfer_out_count + erc20_transfer_self_count, 0),
     )
-    nft_transfer_count = Column(
-        INTEGER,
-        generated=True,
-        server_default=func.coalesce(nft_transfer_in_count + nft_transfer_out_count + nft_transfer_self_count, 0),
-    )
+    nft_transfer_count = Column(INTEGER)
