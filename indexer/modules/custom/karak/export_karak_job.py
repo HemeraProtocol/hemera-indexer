756331d45db8a704b806c94e9c31cbeb8f7ae608
will0x0909 <zhiyuan@thehemera.com>
2024-09-19 15:48:45 +0800

    init commit

---
+++ indexer/modules/custom/karak/export_karak_job.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  2024/9/19 15:19
+# @Author  will
+# @File  export_karak_job.py
+# @Brief
---
+++ indexer/modules/custom/karak/models/af_karak_address_current.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  2024/9/19 15:24
+# @Author  will
+# @File  af_karak_address_current.py
+# @Brief
