4ced5b0afe0051cda5a92bdbf32d4e45521295b9
rkrkrkk <zhufeng@thehemera.com>
2024-09-25 18:21:23 +0800

    format

--- indexer/modules/custom/erc721_token_mint/export_erc721_token_mint_job.py
+++ indexer/modules/custom/erc721_token_mint/export_erc721_token_mint_job.py
@@ -2,17 +2,13 @@ import logging
 from typing import List
 
 from indexer.domain.log import Log
+from indexer.domain.token_transfer import TokenTransfer, extract_transfer_from_log, transfer_event
 from indexer.domain.transaction import Transaction
 from indexer.jobs.filter_transaction_data_job import FilterTransactionDataJob
 from indexer.modules.custom.erc721_token_mint.domain.erc721_mint_time import ERC721TokenMint
+from indexer.specification.specification import TopicSpecification, TransactionFilterByLogs
 from indexer.utils.multicall_hemera.util import calculate_execution_time
 from indexer.utils.utils import ZERO_ADDRESS
-from indexer.domain.token_transfer import (
-    TokenTransfer,
-    extract_transfer_from_log,
-    transfer_event,
-)
-from indexer.specification.specification import TopicSpecification, TransactionFilterByLogs
 
 logger = logging.getLogger(__name__)
 
@@ -24,8 +20,11 @@ def _filter_mint_tokens(logs: List[Log]) -> List[TokenTransfer]:
     token_transfers = []
     for log in logs:
         token_transfers += extract_transfer_from_log(log)
-    return [transfer for transfer in token_transfers if
-            transfer.from_address == ZERO_ADDRESS and transfer.token_type == "ERC721"]
+    return [
+        transfer
+        for transfer in token_transfers
+        if transfer.from_address == ZERO_ADDRESS and transfer.token_type == "ERC721"
+    ]
 
 
 class ExportERC721MintTimeJob(FilterTransactionDataJob):
@@ -47,8 +46,9 @@ class ExportERC721MintTimeJob(FilterTransactionDataJob):
                 token_id=token.value,
                 block_timestamp=token.block_timestamp,
                 block_number=token.block_number,
-                transaction_hash=token.transaction_hash
-            ) for token in mint_tokens
+                transaction_hash=token.transaction_hash,
+            )
+            for token in mint_tokens
         ]
         self._collect_domains(erc721_mint_infos)
 
@@ -57,9 +57,7 @@ class ExportERC721MintTimeJob(FilterTransactionDataJob):
 
     def get_filter(self):
         topic_filter_list = [
-            TopicSpecification(
-                addresses=TARGET_TOKEN_ADDRESS, topics=[transfer_event.get_signature()]
-            )
+            TopicSpecification(addresses=TARGET_TOKEN_ADDRESS, topics=[transfer_event.get_signature()])
         ]
 
         return TransactionFilterByLogs(topic_filter_list)
