c77b52e09cf505498a6199f8206c3ea4ef261f04
zihao.xu <zihao.xu@thehemera.com>
2024-10-11 17:57:13 +0800

    uniform converting between hex-str and bytes

--- api/app/db_service/traces.py
+++ api/app/db_service/traces.py
@@ -1,10 +1,11 @@
 from common.models import db
 from common.models.traces import Traces
 from common.utils.db_utils import build_entities
+from common.utils.format_utils import hex_str_to_bytes
 
 
 def get_traces_by_transaction_hash(transaction_hash, columns="*"):
-    transaction_hash = bytes.fromhex(transaction_hash[2:])
+    transaction_hash = hex_str_to_bytes(transaction_hash)
     entities = build_entities(Traces, columns)
 
     traces = (
