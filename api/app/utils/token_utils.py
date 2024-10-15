e60390c106fb735808d56db056f19f77eae49900
rkrkrkk <zhufeng@thehemera.com>
2024-10-15 17:08:38 +0800

    add price auth

--- api/app/utils/token_utils.py
+++ api/app/utils/token_utils.py
@@ -1,19 +1,27 @@
+import logging
 import os
 import requests
 
 from api.app.cache import cache
 from decimal import Decimal
 
-base_price_url = os.getenv('BASE_PRICE_HOST', default="https://api.socialscan.io/w3w-price")
+logger = logging.getLogger(__name__)
+
+base_price_url = os.getenv('BASE_PRICE_HOST', default="https://api.socialscan.io/price-test/v1/token/price")
+price_auth = os.getenv('PRICE_AUTHENTICATION', '')
+if not price_auth:
+    logger.warning("PRICE_AUTHENTICATION is not set, price will not be fetched")
 
 
 @cache.memoize(300)
 def get_token_price(symbol, date=None) -> Decimal:
+    if not price_auth:
+        return Decimal(0.0)
     params = {'symbol': symbol}
     if date:
         params['date'] = date.isoformat()
 
-    response = requests.get(base_price_url, params=params)
+    response = requests.get(base_price_url, params=params, headers={"Authorization": price_auth})
     response.raise_for_status()
     data = response.json()
     return Decimal(data['price'])
