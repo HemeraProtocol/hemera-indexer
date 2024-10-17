6ebc4a7c6b2be970f8b6288060c35c2104c91205
zihao.xu <zihao.xu@thehemera.com>
2024-10-17 19:25:28 +0800

    validate data in unit test

--- indexer/tests/jobs/test_export_blocks_job.py
+++ indexer/tests/jobs/test_export_blocks_job.py
@@ -3,6 +3,7 @@ import pytest
 from indexer.controller.scheduler.job_scheduler import JobScheduler
 from indexer.domain.block import Block
 from indexer.exporters.console_item_exporter import ConsoleItemExporter
+from indexer.jobs.base_job import BaseJob
 from indexer.tests import ETHEREUM_PUBLIC_NODE_RPC_URL
 from indexer.utils.provider import get_provider_from_uri
 from indexer.utils.thread_local_proxy import ThreadLocalProxy
@@ -29,5 +30,30 @@ def test_export_job():
         start_block=20273057,
         end_block=20273058,
     )
+
+    data_buff = job_scheduler.get_data_buff()
+
+    assert len(data_buff.keys()) == 3
+
+    assert 'block' in data_buff
+    assert len(data_buff['block']) == 2
+    block = data_buff['block'][0]
+    assert block.number == 20273057
+    assert block.hash == '0x23466a3906f8be59ce7a0dd0d6ef084f5bcba03386cae49135054fc1d7f03fa0'
+    assert block.parent_hash == '0xba166c762e417d338838427251481557f734e53cf5f61cc034d9701c3360b948'
+    assert block.extra_data == '0x546974616e2028746974616e6275696c6465722e78797a29'
+    assert len(block.transactions) == 167
+
+    assert 'transaction' in data_buff
+    assert len(data_buff['transaction']) == 350
+    transaction = data_buff['transaction'][183]
+    assert transaction.hash == '0xb0f6f719e4f68df5ebc191ee9ddd9d2cf5a03770c58ca042e719b99a22a43bfd'
+    assert transaction.input[:10] == '0x9950c080'
+    assert transaction.receipt is None
+
+    assert 'block_ts_mapper' in data_buff
+    assert len(data_buff['block_ts_mapper']) == 1
+    assert data_buff['block_ts_mapper'][0].block_number == 20273057
+    assert data_buff['block_ts_mapper'][0].timestamp == 1720576800
 
     job_scheduler.clear_data_buff()
--- indexer/tests/jobs/test_export_transactions_job.py
+++ indexer/tests/jobs/test_export_transactions_job.py
@@ -30,4 +30,21 @@ def test_export_transaction_job():
         end_block=20273058,
     )
 
+    data_buff = job_scheduler.get_data_buff()
+
+    assert len(data_buff.keys()) == 4
+    assert 'block' in data_buff.keys()
+    assert 'transaction'in data_buff.keys()
+    assert 'log' in data_buff.keys()
+
+    assert len(data_buff['log']) == 827
+    log = data_buff['log'][511]
+    assert log.transaction_index == 21
+    assert log.log_index == 103
+    assert log.block_hash == '0x6d5644ab134fe01595e3e0628fe4945a47666f7dba251c2258b99ed7b1705585'
+    assert log.transaction_hash == '0xd6e2b0bdb45895c68de4d62f1976c76c7e3583f86244cf17ca1a5d5bf152646e'
+    assert log.address == '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+    assert log.topic0 == '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
+    assert log.topic3 is None
+
     job_scheduler.clear_data_buff()
