6a207bd23cfba2de79b652c0ca3741574cba4ca9
will0x0909 <zhiyuan@thehemera.com>
2024-10-22 15:48:12 +0800

    aave stash

--- indexer/modules/custom/aave_v2/models/aave_v2_lending_address_current.py
+++ indexer/modules/custom/aave_v2/models/aave_v2_lending_address_current.py
@@ -1,15 +1,22 @@
-from datetime import datetime
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time  2024/10/22 11:33
+# @Author  will
 
-from sqlalchemy import Column, Index, PrimaryKeyConstraint, desc, func, text
-from sqlalchemy.dialects.postgresql import BIGINT, BOOLEAN, BYTEA, INTEGER, NUMERIC, TIMESTAMP, VARCHAR
+from sqlalchemy import Column, func, BOOLEAN, text, PrimaryKeyConstraint
+from sqlalchemy.dialects.mysql import BIGINT
+from sqlalchemy.dialects.postgresql import BYTEA, TIMESTAMP
 
 from common.models import HemeraModel, general_converter
 
 
-class AaveV2LendingFactorCurrent(HemeraModel):
-    __tablename__ = "af_aave_v2_change_factor_current"
-    asset_address = Column(BYTEA, primary_key=True)
-    factor = Column(BIGINT)
+class AaveV2AddressCurrent(HemeraModel):
+    __tablename__ = "af_aave_v2_address_current"
+    address = Column(BYTEA, primary_key=True)
+    reserve = Column(BYTEA, primary_key=True)
+    supply = Column(BIGINT)
+    borrow = Column(BIGINT)
+    repay = Column(BIGINT)
 
     block_number = Column(BIGINT)
     block_timestamp = Column(BIGINT)
@@ -18,7 +25,7 @@ class AaveV2LendingFactorCurrent(HemeraModel):
     update_time = Column(TIMESTAMP, server_default=func.now())
     reorg = Column(BOOLEAN, server_default=text("false"))
 
-    __table_args__ = (PrimaryKeyConstraint("asset_address"),)
+    __table_args__ = (PrimaryKeyConstraint("address", "reserve"),)
 
     @staticmethod
     def model_domain_mapping():
