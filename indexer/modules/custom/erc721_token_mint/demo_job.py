0f229c98e464c3c2a57093f75ed9776810e770f3
godshan.eth <shuo.shan@thehemera.com>
2024-10-23 23:36:51 +0800

    Format code

--- indexer/modules/custom/erc721_token_mint/demo_job.py
+++ indexer/modules/custom/erc721_token_mint/demo_job.py
@@ -1,21 +1,19 @@
 import logging
 from typing import List
 
-from indexer.jobs.base_job import FilterTransactionDataJob
+from common.utils.web3_utils import ZERO_ADDRESS
 
 # Dependency dataclass
 from indexer.domain.log import Log
 from indexer.domain.token_transfer import TokenTransfer, extract_transfer_from_log
-
-# Utility
-from indexer.utils.abi_setting import ERC721_TRANSFER_EVENT
-from indexer.specification.specification import TopicSpecification, TransactionFilterByLogs
+from indexer.jobs.base_job import FilterTransactionDataJob
 
 # Custom dataclass
 from indexer.modules.custom.erc721_token_mint.domain.erc721_mint_time import ERC721TokenMint
+from indexer.specification.specification import TopicSpecification, TransactionFilterByLogs
 
-
-from common.utils.web3_utils import ZERO_ADDRESS
+# Utility
+from indexer.utils.abi_setting import ERC721_TRANSFER_EVENT
 
 logger = logging.getLogger(__name__)
 
@@ -33,7 +31,7 @@ def _filter_mint_tokens(logs: List[Log]) -> List[TokenTransfer]:
 
 class DemoJob(FilterTransactionDataJob):
     # Declare existing dataclass you may need for your job
-    # The indexer will automatically run other jobs and prepare the dataclass 
+    # The indexer will automatically run other jobs and prepare the dataclass
     dependency_types = [Log]
 
     # This is to declare output dataclass your job outputs
@@ -46,13 +44,10 @@ class DemoJob(FilterTransactionDataJob):
         super().__init__(**kwargs)
         self._contract_list = self.user_defined_config.get("contract_address")
         self.logger.info("Contracts to process %s", self._contract_list)
-    
+
     def get_filter(self):
         topic_filter_list = [
-            TopicSpecification(
-                addresses=self._contract_list, 
-                topics=[ERC721_TRANSFER_EVENT.get_signature()]
-            )
+            TopicSpecification(addresses=self._contract_list, topics=[ERC721_TRANSFER_EVENT.get_signature()])
         ]
 
         return TransactionFilterByLogs(topic_filter_list)
