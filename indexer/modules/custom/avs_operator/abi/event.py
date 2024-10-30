22388631f475b7b3e27deaa6a891aedce338f552
zhufeng <zhufeng@thehemera.com>
2024-10-30 14:47:21 +0800

    avs operator

---
+++ indexer/modules/custom/avs_operator/abi/event.py
@@ -0,0 +1,60 @@
+from common.utils.abi_code_utils import Event
+
+
+class IndexRecordEvent(Event):
+    def __init__(self):
+        super().__init__(
+            {
+                "anonymous": False,
+                "inputs": [
+                    {
+                        "indexed": False,
+                        "internalType": "uint256",
+                        "name": "chainId",
+                        "type": "uint256"
+                    },
+                    {
+                        "indexed": False,
+                        "internalType": "uint256",
+                        "name": "startBlock",
+                        "type": "uint256"
+                    },
+                    {
+                        "indexed": False,
+                        "internalType": "uint256",
+                        "name": "endBlock",
+                        "type": "uint256"
+                    },
+                    {
+                        "indexed": False,
+                        "internalType": "bytes32",
+                        "name": "codeHash_",
+                        "type": "bytes32"
+                    },
+                    {
+                        "components": [
+                            {
+                                "internalType": "bytes4",
+                                "name": "dataClass",
+                                "type": "bytes4"
+                            },
+                            {
+                                "internalType": "uint256",
+                                "name": "count",
+                                "type": "uint256"
+                            },
+                            {
+                                "internalType": "bytes32",
+                                "name": "dataHash",
+                                "type": "bytes32"
+                            }
+                        ],
+                        "indexed": False,
+                        "internalType": "struct HemeraHistoryTransparency.IndexerOutput",
+                        "name": "outputs",
+                        "type": "tuple"
+                    }
+                ],
+                "name": "indexRecord",
+                "type": "event"
+            })
---
+++ indexer/modules/custom/avs_operator/bn254/sign.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+
+from py_ecc.bn128 import (
+    multiply,
+    field_modulus,
+    FQ,
+)
+
+from common.utils.format_utils import bytes_to_hex_str
+
+
+def parse_solc_G1(solc_G1: Tuple[int, int]):
+    x, y = solc_G1
+    return FQ(x), FQ(y)
+
+
+def map_to_curve(x: bytes) -> tuple:
+    x_int = int.from_bytes(x, byteorder='big') % field_modulus
+
+    while True:
+        beta, y = find_y_from_x(x_int)
+
+        if beta == (y ** 2) % field_modulus:
+            return (x_int, y)
+
+        x_int = (x_int + 1) % field_modulus
+
+    return (0, 0)
+
+
+def find_y_from_x(x: int) -> tuple:
+    # beta = (x^3 + b) % p
+    b = 3
+    beta = (x ** 3 + b) % field_modulus
+
+    # hard code, keep consistent with on-chain parameters, https://github.com/Layr-Labs/eigenlayer-middleware/blob/7d49b5181b09198ed275783453aa082bb3766990/src/libraries/BN254.sol#L324
+    exponent = int("0xc19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52", 16)
+    y = pow(beta, exponent, field_modulus)
+
+    return (beta, y)
+
+
+def sign_message(private_key, message):
+    point_on_curve = map_to_curve(message)
+    signature = multiply(parse_solc_G1(point_on_curve), private_key)
+    signature_bytes = b''.join(int(x).to_bytes(32, 'big') for x in signature)
+    signed = bytes_to_hex_str(signature_bytes)
+    return signed
