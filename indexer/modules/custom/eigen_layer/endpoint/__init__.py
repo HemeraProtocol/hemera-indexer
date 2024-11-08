79a5105f65340466b766df7fa33f28f7433177ea
li xiang <leeideal93@gmail.com>
2024-11-08 13:25:11 +0800

    modify eigen layerjob (#223)
    * Modify eigen layerjob
    
    * Add endpoint

---
+++ indexer/modules/custom/eigen_layer/endpoint/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from flask_restx.namespace import Namespace
+
+eigen_layer_namespace = Namespace("Eigen Layer Namespace", path="/", description="Eigen Layer API")
