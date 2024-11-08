7cf61f2fd7bdf85bbc54c314fb391171f5848b65
zhufeng <zhufeng@thehemera.com>
2024-11-08 18:06:48 +0800

    update

--- indexer/modules/custom/avs_operator/bn128/sign.py
+++ indexer/modules/custom/avs_operator/bn128/sign.py
@@ -4,6 +4,9 @@ from py_ecc.bn128 import FQ, field_modulus, multiply
 
 from common.utils.format_utils import bytes_to_hex_str
 
+# keep consistent with on-chain parameters, https://github.com/Layr-Labs/eigenlayer-middleware/blob/7d49b5181b09198ed275783453aa082bb3766990/src/libraries/BN254.sol#L324
+exponent = int("0xc19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52", 16)
+
 
 def parse_solc_G1(solc_G1: Tuple[int, int]):
     x, y = solc_G1
@@ -28,11 +31,7 @@ def find_y_from_x(x: int) -> tuple:
     # beta = (x^3 + b) % p
     b = 3
     beta = (x**3 + b) % field_modulus
-
-    # hard code, keep consistent with on-chain parameters, https://github.com/Layr-Labs/eigenlayer-middleware/blob/7d49b5181b09198ed275783453aa082bb3766990/src/libraries/BN254.sol#L324
-    exponent = int("0xc19139cb84c680a6e14116da060561765e05aa45a1c72a34f082305b61f3f52", 16)
     y = pow(beta, exponent, field_modulus)
-
     return (beta, y)
 
 
