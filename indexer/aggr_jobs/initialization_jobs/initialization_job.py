258cd6ffe62c5cfa2a1ca88ce77a0d05f798fb69
river <river.zeng@thehemera.com>
2024-11-26 20:05:02 +0800

    add init balance job

--- indexer/aggr_jobs/initialization_jobs/initialization_job.py
+++ indexer/aggr_jobs/initialization_jobs/initialization_job.py
@@ -8,6 +8,9 @@ class InitializationJob:
         config = kwargs["config"]
         self.db_service = config["db_service"]
         self.dblink_url = config["dblink_url"]
+        job_dict = config['jobs_dict']
+        self.if_init_balance = job_dict.get('if_init_balance', False)
+        pass
 
     def init_token_price(self):
         token_price_sql_template = """
@@ -50,9 +53,39 @@ class InitializationJob:
 
         session.close()
 
+    def totally_init_period_address_token_balance(self, start_date):
+        session = self.db_service.Session()
+
+        sql_template = f"""
+        truncate table period_address_token_balances;
+        insert into period_address_token_balances(address, token_address, token_id, token_type, balance, period_date)
+        select address,
+               token_address,
+               token_id,
+               token_type,
+               balance,
+               date(block_timestamp) as period_date
+        from (select *,
+                     row_number() over (partition by address,token_address,token_id order by block_timestamp desc) as rn
+              from address_token_balances
+              WHERE block_timestamp < '{start_date}') t
+        where rn = 1;        
+        """
+        start_time = time.time()
+        session.execute(text(sql_template))
+
+        session.commit()
+        execution_time = time.time() - start_time
+        print(f'----------- executed in {execution_time:.2f} seconds: totally_init_period_address_token_balance')
+
+        session.close()
+
     def run(self, **kwargs):
         self.start_date = kwargs["start_date"]
         self.end_date = kwargs["end_date"]
 
         self.init_token_price()
         self.init_period_address_token_balance()
+
+        if self.if_init_balance:
+            self.totally_init_period_address_token_balance(self.start_date)
