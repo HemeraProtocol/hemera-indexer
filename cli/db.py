1652d949a1e1675ce9896d01c62e66d90b6f1b13
zihao.xu <zihao.xu@thehemera.com>
2024-12-04 11:56:49 +0800

    add truncate table in db command line

--- cli/db.py
+++ cli/db.py
@@ -2,6 +2,7 @@ import logging
 from typing import List
 
 import click
+from sqlalchemy import text
 
 from common.models import HemeraModel, model_path_patterns
 from common.services.postgresql_service import PostgreSQLService
@@ -34,14 +35,14 @@ logger = logging.getLogger("DB Client")
     show_default=True,
     required=False,
     help="The database version that would be initialized. "
-    "By using default value 'head', database would be initialized to the latest version."
-    "To make this option work, either -i or --init must be used.",
+         "By using default value 'head', database would be initialized to the latest version."
+         "To make this option work, either -i or --init must be used.",
 )
 @click.option(
     "-c",
     "--create-tables",
     type=str,
-    default="",
+    default=None,
     required=False,
     help="Table names that need to be created in the database. e.g. blocks,transactions",
 )
@@ -49,14 +50,22 @@ logger = logging.getLogger("DB Client")
     "-d",
     "--drop-tables",
     type=str,
-    default="",
+    default=None,
     required=False,
     help="Table names that need to be dropped in the database. e.g. blocks,transactions",
 )
-def db(postgres_url, init, version, create_tables="", drop_tables=""):
+@click.option(
+    "-t",
+    "--truncate-tables",
+    type=str,
+    default=None,
+    required=False,
+    help="Table names that need to clean up data. e.g. blocks,transactions",
+)
+def db(postgres_url, init, version, create_tables, drop_tables, truncate_tables):
     service = PostgreSQLService(jdbc_url=postgres_url, db_version=version, init_schema=init)
 
-    if create_tables != "" or drop_tables != "":
+    if create_tables or drop_tables or truncate_tables:
         exist_models_path = [
             value["cls_import_path"]
             for key, value in scan_subclass_by_path_patterns(model_path_patterns, HemeraModel).items()
@@ -67,17 +76,26 @@ def db(postgres_url, init, version, create_tables="", drop_tables=""):
             if hasattr(table, "__tablename__")
         }
 
-        tables = create_tables.split(",")
-        if len(tables) > 0:
-            create(service, tables, exist_models)
+        if create_tables:
+            tables = create_tables.split(",")
+            if len(tables) > 0:
+                create(service, tables, exist_models)
 
-        logger.info("Table creation has been finished.")
+            logger.info("Table creation has been finished.")
 
-        tables = drop_tables.split(",")
-        if len(drop_tables) > 0:
-            drop(service, tables, exist_models)
+        if drop_tables:
+            tables = drop_tables.split(",")
+            if len(tables) > 0:
+                drop(service, tables, exist_models)
 
-        logger.info("Table deletion has been finished.")
+            logger.info("Table deletion has been finished.")
+
+        if truncate_tables:
+            tables = truncate_tables.split(",")
+            if len(tables) > 0:
+                truncate(service, tables, exist_models)
+
+            logger.info("Table cleanup has been finished.")
 
     logger.info("db operation finished, now exit.")
 
@@ -104,3 +122,17 @@ def drop(service: PostgreSQLService, tables: List[str], exist_models: dict):
             logger.warning(
                 f"No Table {table} model definition was found in the following directories: {model_path_patterns} "
             )
+
+
+def truncate(service: PostgreSQLService, tables: List[str], exist_models: dict):
+    session = service.get_service_session()
+    for table in tables:
+        if table in exist_models:
+            session.execute(text(f"TRUNCATE TABLE {table}"))
+            session.commit()
+            logger.info(f"Table {table} truncated successfully.")
+        else:
+            logger.warning(
+                f"No Table {table} model definition was found in the following directories: {model_path_patterns} "
+            )
+    session.close()
