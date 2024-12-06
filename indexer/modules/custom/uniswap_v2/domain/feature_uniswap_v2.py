155985dbaf2216e650277b8260282923163ed32c
river <river.zeng@thehemera.com>
2024-12-06 10:48:06 +0800

    delete legacy code for uniswapv2

--- indexer/modules/custom/uniswap_v2/domain/feature_uniswap_v2.py
+++ indexer/modules/custom/uniswap_v2/domain/feature_uniswap_v2.py
@@ -4,16 +4,6 @@ from indexer.domain import FilterData
 
 
 @dataclass
-class UniswapV2Pool_(FilterData):
-    factory_address: str
-    pool_address: str
-    token0_address: str
-    token1_address: str
-    length: int
-    called_block_number: int
-
-
-@dataclass
 class UniswapV2Pool(FilterData):
     factory_address: str
     pool_address: str
