6c333eef5014f5d9f0ddc096883664505435357e
zihao.xu <zihao.xu@thehemera.com>
2024-12-09 19:52:34 +0800

    assemble db data as specified udf domain

--- common/utils/db_utils.py
+++ common/utils/db_utils.py
@@ -1,7 +1,11 @@
+from typing import List
+
 from sqlalchemy import text
 
-from common.models import db
+from common.models import HemeraModel, db
+from common.services.postgresql_service import PostgreSQLService
 from common.utils.config import get_config
+from indexer.domain import Domain, dict_to_dataclass
 
 app_config = get_config()
 
@@ -30,3 +34,31 @@ def get_total_row_count(table):
         )
     ).fetchone()
     return estimate_transaction[0]
+
+
+def require_data_as_domain(
+    service: PostgreSQLService, table: HemeraModel, columns: List[str], domain: Domain
+) -> List[Domain]:
+    session = service.get_service_session()
+
+    entities = build_entities(table, columns)
+
+    try:
+        datas = session.query(table).with_entities(*entities).all()
+    finally:
+        session.close()
+
+    domains = [dict_to_dataclass(data, domain) for data in datas]
+    return domains
+
+
+def build_domains_by_sql(service: PostgreSQLService, domain: Domain, sql: str) -> List[Domain]:
+    session = service.get_service_session()
+
+    try:
+        datas = session.execute(text(sql))
+    finally:
+        session.close()
+
+    domains = [dict_to_dataclass(data, domain) for data in datas]
+    return domains
