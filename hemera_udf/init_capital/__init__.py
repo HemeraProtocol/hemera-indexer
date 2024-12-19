0b8944ac5b6042f0c72af104c7d6f8b245ce5a74
river <river.zeng@thehemera.com>
2024-12-19 15:12:35 +0800

    add mantle config

--- hemera_udf/init_capital/__init__.py
+++ hemera_udf/init_capital/__init__.py
@@ -8,5 +8,28 @@ __all__ = ["__version__"]
 
 __version__ = "0.1.0"
 
+from hemera.common.enumeration.entity_type import DynamicEntityTypeRegistry
+from hemera_udf.init_capital.domains import (
+    InitCapitalPoolHistoryDomain,
+    InitCapitalPoolUpdateDomain,
+    InitCapitalPositionCreateDomain,
+    InitCapitalPositionHistoryDomain,
+    InitCapitalPositionUpdateDomain,
+    InitCapitalRecordDomain,
+)
+
 if packaging.version.parse(packaging.version.parse(hemera_version).base_version) < packaging.version.parse("1.0.0"):
     raise RuntimeError(f"The package `hemera-modules-xxx:{__version__}` needs Hemera 1.0.0+")
+
+value = DynamicEntityTypeRegistry.register("INIT_CAPITAL")
+DynamicEntityTypeRegistry.register_output_types(
+    value,
+    {
+        InitCapitalPoolHistoryDomain,
+        InitCapitalPoolUpdateDomain,
+        InitCapitalPositionCreateDomain,
+        InitCapitalPositionHistoryDomain,
+        InitCapitalPositionUpdateDomain,
+        InitCapitalRecordDomain,
+    },
+)
--- hemera_udf/merchant_moe/__init__.py
+++ hemera_udf/merchant_moe/__init__.py
@@ -8,6 +8,7 @@ __all__ = ["__version__"]
 
 __version__ = "0.1.0"
 
+from hemera.common.enumeration.entity_type import DynamicEntityTypeRegistry
 from hemera_udf.merchant_moe.domains import (
     MerchantMoeErc1155TokenCurrentHolding,
     MerchantMoeErc1155TokenCurrentSupply,
@@ -20,20 +21,21 @@ from hemera_udf.merchant_moe.domains import (
     MerchantMoeTokenCurrentBin,
 )
 
-from hemera.common.enumeration.entity_type import DynamicEntityTypeRegistry
-
 if packaging.version.parse(packaging.version.parse(hemera_version).base_version) < packaging.version.parse("1.0.0"):
     raise RuntimeError(f"The package `hemera-modules-xxx:{__version__}` needs Hemera 1.0.0+")
 
 value = DynamicEntityTypeRegistry.register("MERCHANTMOE")
 DynamicEntityTypeRegistry.register_output_types(
-    value, {MerchantMoeErc1155TokenHolding,
-            MerchantMoeErc1155TokenCurrentHolding,
-            MerchantMoeErc1155TokenSupply,
-            MerchantMoeErc1155TokenCurrentSupply,
-            MerchantMoeTokenBin,
-            MerchantMoeTokenCurrentBin,
-            MerchantMoePool,
-            MerchantMoePoolCurrentStatus,
-            MerchantMoePoolRecord}
+    value,
+    {
+        MerchantMoeErc1155TokenHolding,
+        MerchantMoeErc1155TokenCurrentHolding,
+        MerchantMoeErc1155TokenSupply,
+        MerchantMoeErc1155TokenCurrentSupply,
+        MerchantMoeTokenBin,
+        MerchantMoeTokenCurrentBin,
+        MerchantMoePool,
+        MerchantMoePoolCurrentStatus,
+        MerchantMoePoolRecord,
+    },
 )
