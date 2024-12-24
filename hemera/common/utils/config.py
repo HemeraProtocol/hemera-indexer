21f45d28261b86a5df774cb45325a4ce75ec69d0
zihao.xu <zihao.xu@thehemera.com>
2024-12-24 14:02:02 +0800

    log warning while environment variable checking

--- hemera/common/utils/config.py
+++ hemera/common/utils/config.py
@@ -69,3 +69,8 @@ def check_and_set_default_env(key: str, default_value: str):
     env_value = os.environ.get(key)
     if env_value is None:
         os.environ[key] = default_value
+    else:
+        logging.warning(
+            f"The environment variable: {key} has been set to `{env_value}`. "
+            f"Please confirm that {key} assignment meets your expectations."
+        )
