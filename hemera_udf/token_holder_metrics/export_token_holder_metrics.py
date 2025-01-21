5071ae394babd2767ce890e806c2f2333fbb625a
zhufeng <zhufeng@thehemera.com>
2025-01-21 12:33:42 +0800

    update

--- hemera_udf/token_holder_metrics/export_token_holder_metrics.py
+++ hemera_udf/token_holder_metrics/export_token_holder_metrics.py
@@ -256,7 +256,7 @@ class ExportTokenHolderMetricsJob(ExtensionJob):
         start_time = time.time()
         logger.info(f"Starting to process {len(address_token_pairs)} address-token pairs")
 
-        BATCH_SIZE = 500
+        BATCH_SIZE = 1000
         result = {}
         session = self._service.get_service_session()
 
