7290e1d11b538c93b47cfc69c7b4f2e884884b45
ideal93 <leeideal93@gmail.com>
2025-01-24 18:14:55 +0800

    delete token_symbol

--- hemera/indexer/exporters/kafka_exporter.py
+++ hemera/indexer/exporters/kafka_exporter.py
@@ -76,8 +76,7 @@ class KafkaItemExporter(BaseExporter):
                 data.token_id = 0
             return data
         if isinstance(data, DexBlockTokenPrice):
-            if not isinstance(data.token_symbol, str):
-                data.token_symbol = ""
+            data.token_symbol = ""
         if isinstance(data, (TokenHolderMetricsHistoryD, TokenHolderMetricsCurrentD)):
             if data.current_balance:
                 data.current_balance = int(data.current_balance)
