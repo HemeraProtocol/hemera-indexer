ba1be550873d9b47654ca249355f85e0ebd74865
ideal93 <leeideal93@gmail.com>
2025-01-24 18:10:05 +0800

    Try fix data.token_symbol

--- hemera/indexer/exporters/kafka_exporter.py
+++ hemera/indexer/exporters/kafka_exporter.py
@@ -75,6 +75,9 @@ class KafkaItemExporter(BaseExporter):
             if data.token_id is None or data.token_id < 0:
                 data.token_id = 0
             return data
+        if isinstance(data, DexBlockTokenPrice):
+            if not isinstance(data.token_symbol, str):
+                data.token_symbol = ""
         if isinstance(data, (TokenHolderMetricsHistoryD, TokenHolderMetricsCurrentD)):
             if data.current_balance:
                 data.current_balance = int(data.current_balance)
@@ -95,7 +98,6 @@ class KafkaItemExporter(BaseExporter):
                 UniswapV2SwapEvent,
                 UniswapV3SwapEvent,
                 ERC20TokenTransfer,
-                DexBlockTokenPrice,
                 Log,
                 TokenHolderMetricsCurrentD,
                 TokenHolderMetricsHistoryD,
