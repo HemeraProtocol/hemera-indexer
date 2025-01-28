49c353becd619ebf1af9c200a5fdffda00bde1d1
zhufeng <zhufeng@thehemera.com>
2025-01-28 23:41:43 +0800

    update

--- hemera_udf/token_holder_metrics/export_token_holder_metrics.py
+++ hemera_udf/token_holder_metrics/export_token_holder_metrics.py
@@ -393,8 +393,7 @@ class ExportTokenHolderMetricsJob(ExtensionJob):
 
         session = self._service.get_service_session()
         prices = session.execute(price_sql, {"min_block": start_block, "max_block": end_block}).fetchall()
-        session.close()
-        
+
         logger.info("fetch %s  token prices", len(prices))
 
         token_price_maps = {token: SortedDict() for token in self.history_token_prices}
@@ -409,7 +408,8 @@ class ExportTokenHolderMetricsJob(ExtensionJob):
             block_num = price_row[1]
             price = float(price_row[2])
             token_price_maps[token_addr][block_num] = price
-            
+        session.close()
+        
         logger.info("Initialized %s tokens", len(token_price_maps))
 
         self.token_price_maps = token_price_maps
