af9b867b072b59b7926e1856c639ad2ec96db48b
will0x0909 <zhiyuan@thehemera.com>
2025-02-11 14:19:28 +0800

    smart_retry

--- hemera/indexer/utils/multicall_hemera/util.py
+++ hemera/indexer/utils/multicall_hemera/util.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # @Time  2024/8/20 14:13
 # @Author  will
-# @File  util.py.py
+# @File  util.py
 # @Brief
 import atexit
 import logging
@@ -126,7 +126,7 @@ class ThreadPoolManager:
                     cls.check_results(result)
                     results[index] = result
                 except Exception as e:
-                    logger.error(f"Task {index} failed with error: {e}")
+                    # logger.error(f"Task {index} failed with error: {e}")
                     pending_tasks[index] = chunks[index]
 
             if pending_tasks:
@@ -137,6 +137,6 @@ class ThreadPoolManager:
 
         if pending_tasks:
             logger.error(f"Some tasks failed after {max_attempts} retries: {list(pending_tasks.keys())}")
-            # raise Exception(f"Some tasks failed after {max_attempts} retries: {list(pending_tasks.keys())}")
+            raise Exception(f"Some tasks failed after {max_attempts} retries: {list(pending_tasks.keys())}")
 
         return results
