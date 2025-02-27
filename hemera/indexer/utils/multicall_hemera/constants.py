a9088124e41e74cf8d625df57c9fdd0a11b925ec
will0x0909 <zhiyuan@thehemera.com>
2025-02-27 11:54:33 +0800

    add sonic

--- hemera/indexer/utils/multicall_hemera/constants.py
+++ hemera/indexer/utils/multicall_hemera/constants.py
@@ -109,6 +109,8 @@ CYBER_TESTNET = NetworkConfig(111557560, "CyberTestnet")
 CYBER = NetworkConfig(7560, "Cyber", 3413302)
 STORY_ODYSSEY_TESTNET = NetworkConfig(1516, "StoryOdysseyTestnet", 14880)
 MONAD_TESTNET = NetworkConfig(10143, "MonadalTestnet", 251449)
+SONIC = NetworkConfig(146, "Sonic", 60)
+SONIC_TESTNET = NetworkConfig(57054, "SonicTestnet", 1100)
 
 
 def get_multicall_network(chain_id: int) -> NetworkConfig:
